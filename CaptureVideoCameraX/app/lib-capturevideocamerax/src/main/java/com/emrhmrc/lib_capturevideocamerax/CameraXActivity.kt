3aae8a259d425baa3c6e46750b8f5be9bf0ec8ad
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2020-05-31 18:05:58 +0300

    test

---
+++ CaptureVideoCameraX/app/lib-capturevideocamerax/src/main/java/com/emrhmrc/lib_capturevideocamerax/CameraXActivity.kt
@@ -0,0 +1,14 @@
+package com.emrhmrc.capture_camerax
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.emrhmrc.lib_capturevideocamerax.R
+
+class CameraXActivity : AppCompatActivity() {
+
+    //test
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ CaptureVideoCameraX/app/lib-capturevideocamerax/src/main/java/com/emrhmrc/lib_capturevideocamerax/enums/CameraRatio.kt
@@ -0,0 +1,7 @@
+package com.emrhmrc.capture_camerax.enums
+
+enum class CameraRatio {
+    RATIO_1_1,
+    RATIO_4_3,
+    RATIO_SCALE
+}
