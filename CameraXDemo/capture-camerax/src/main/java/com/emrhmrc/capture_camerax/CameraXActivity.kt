361a48a4ef001ba9f515593dd354deb3e7babcfe
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2020-05-31 17:35:18 +0300

    Sadece Video Capture için ayarlamalar yapıldı ve lib olarak yeniden oluşturuldu

--- CameraXDemo/capture-camerax/src/main/java/com/emrhmrc/capture_camerax/CameraXActivity.kt
+++ CameraXDemo/capture-camerax/src/main/java/com/emrhmrc/capture_camerax/CameraXActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class CameraXActivity : AppCompatActivity() {
 
+    //test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
